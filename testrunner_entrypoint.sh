a23b8869d5712b66cca9e0849cc7a5936fedbefb
mamonu <thorosm@msn.com>
2020-08-20 23:30:08 +0100

    trying to get testrunner up and running

--- testrunner_entrypoint.sh
+++ testrunner_entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pytest -x --cov-report term-missing --cov=splink_graph tests/
+pytest -v
 
 if [ $? -ne 0 ]
 then
   exit 1
 fi
 
-export GIT_BRANCH=$(echo $GITHUB_REF | sed "s/refs\/heads\///")
-coveralls
+#export GIT_BRANCH=$(echo $GITHUB_REF | sed "s/refs\/heads\///")
+#coveralls
 exit 0
