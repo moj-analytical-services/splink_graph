d160d9baad3ed47512df0a490cb4559a1ccc2ff6
Mamonu <thorosm@msn.com>
2021-06-15 08:03:36 +0000

    case of many extra columns in bridges taken care of

--- splink_graph/vectorised.py
+++ splink_graph/vectorised.py
@@ -141,6 +141,7 @@ output spark dataframe:
 
         return pd.merge(bpdf, pdf, how="inner", on=[psrc, pdst])
 
+    indf = sparkdf.select(psrc, pdst,pweight,pdistance,pcomponent)
     out = sparkdf.groupby(component).apply(br_p_udf)
     return out
 
