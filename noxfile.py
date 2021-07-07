c0f87fbb980c48781b1dec230a866397cb6baf99
mamonu <thorosm@msn.com>
2021-07-07 23:05:55 +0100

    changed toxfile to install newest pip

--- noxfile.py
+++ noxfile.py
@@ -3,13 +3,14 @@ import nox
 @nox.parametrize("pyspark", ["2.4.5", "3.0.1"])
 @nox.parametrize("pandas", ["0.25.3", "1.0.1"])
 def tests(session,pandas, pyspark):
+    session.run("python", "-m", "pip", "install", "--upgrade", "pip")
     session.install('pytest')
     session.install(f"pandas=={pandas}")
     session.install('numpy==1.19.5', "--no-deps")
     if pyspark=="2.4.5":
-        session.install('pyarrow==0.14.1', "--no-deps")
+        session.install('pyarrow==0.14.1')
     else:
-        session.install('pyarrow==0.15.1', "--no-deps")
+        session.install('pyarrow==0.15.1')
     session.install("networkx==2.5.1")
     session.install(f"pyspark=={pyspark}")
     session.run("pytest", "-v","-W", "ignore::DeprecationWarning")
