4b00ceeae62fa93f3775c6ce1e6d7651686be0d4
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2021-07-22 11:27:33 +0100

    should finally work

--- tests/test_node_metrics.py
+++ tests/test_node_metrics.py
@@ -40,7 +40,7 @@ def test_eigencentrality_customcolname(spark):
     assert df_result["eigen_centrality"][0] == pytest.approx(0.50000, 0.01)
 
     f1 = df_result["node_id"] == "a"
-    assert df_result.loc[f1, "clus_id"] == 1
+    assert df_result.loc[f1, "clus_id"][0] == 1
 
 
 def test_harmoniccentrality_simple(spark):
